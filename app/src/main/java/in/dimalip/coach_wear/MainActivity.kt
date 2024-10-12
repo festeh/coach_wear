37ce3b79bd5c1c0ac04a3964d62c56a16f5f61fe
Dmitrii Lipin <dmitrii.lipin@projectx.ai>
2024-10-12 21:41:50 +0200

    upd

--- app/src/main/java/in/dimalip/coach_wear/MainActivity.kt
+++ app/src/main/java/in/dimalip/coach_wear/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import `in`.dimalip.coach_wear.ui.theme.Coach_wearTheme
@@ -16,7 +18,7 @@ import `in`.dimalip.coach_wear.ui.theme.Coach_wearTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             Coach_wearTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -32,10 +34,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
     )
+    {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
